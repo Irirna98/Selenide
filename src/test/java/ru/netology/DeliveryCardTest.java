30007c0d4f86bf7e754e8d3a71785dc35844d24f
Irirna <burdina98@mail.ru>
2023-06-05 10:57:02 +0500

    init

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -26,7 +26,7 @@ public class DeliveryCardTest {
         String getText;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Пермь");
-        String currentDate = generateDate(4, "dd,MM,yyyy");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Василий-Васильевич Василий");
@@ -35,6 +35,6 @@ public class DeliveryCardTest {
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на 06.06.2023"));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
 }
